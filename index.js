1d5b954857cdb3b9b2946019c12bc6cf36d33f92
Abhishek Verma <vermaabhimanyu593@gmail.com>
2023-10-15 21:15:56 +0530

    portfolio added to github

---
+++ index.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text",{
+    strings:["Frontend Developer", "Backend Developer", "Competitive Programmar", "Upcoming Software Engineer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
